26eaf35702994b94a8223d9928a3d4be9716e959
Diviko04 <suan.adoney1999@gmail.com>
2025-03-06 14:26:12 +0000

    primer home

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/Cocktails">Cocktails</RouterLink>
-        <RouterLink to="/Deserts">Deserts</RouterLink>
         <RouterLink to="/Menus">Menus</RouterLink>
+        <RouterLink to="/Recommendations">Recommendations</RouterLink>
       </nav>
     </div>
   </header>
@@ -24,20 +20,19 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+  /* line-height: 1.5;
+  max-height: 100vh; */
 }
 
 nav {
   width: 100%;
-  font-size: 12px;
+  font-size: 25px;
   text-align: center;
   margin-top: 2rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
 }
 
 nav a.router-link-exact-active {
@@ -58,7 +53,7 @@ nav a:first-of-type {
   border: 0;
 }
 
-@media (min-width: 1024px) {
+/* @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
@@ -72,7 +67,7 @@ nav a:first-of-type {
   header .wrapper {
     display: flex;
     place-items: flex-start;
-    flex-wrap: wrap;
+    flex-wrap: row;
   }
 
   nav {
@@ -83,5 +78,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
