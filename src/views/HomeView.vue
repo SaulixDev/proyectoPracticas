9ad3197b9499f9da58c27a3f303a3f900510ba1a
Diviko04 <suan.adoney1999@gmail.com>
2025-03-06 15:34:45 +0000

    de home a homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
+<template>
+  <div>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      Consectetur corporis ipsum quaerat eligendi rem dolor accusamus.
+      Consectetur ex debitis dolores dignissimos quia at soluta hic corporis.
+      Recusandae voluptatibus obcaecati sit?
+    </p>
+  </div>
+</template>
+
 <script setup>
-import Home from '../components/Home.vue'
+
 </script>
 
-<template>
-  <main>
-    <Home />
-  </main>
-</template>
+<style scoped>
+  div{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+  }
+</style>
