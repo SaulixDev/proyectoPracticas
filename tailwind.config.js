8e7df4142b665d799d266f6e71fb82f5b797afa3
Saul <saulojedaalonso@gmail.com>
2025-03-10 10:27:44 +0000

    Tailwind arreglado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,7 @@ module.exports = {
     theme: {
       extend: {
         colors: {
-            'primary-100': '#D2B48C',
-            'primary-200':'#b49770',
-            'primary-300' : '#705834',
-            'accent-100' : '#FFE4C4',
-            'accent-200' : '#9a8366',
-            'text-100' : '#483C32',
-            'text-200' : '#74665b',
-            'bg-100' : '#F5F5DC',
-            'bg-200' : '#ebebd2',
-            'bg-300' : '#c2c2aa',
+         
         },
       },
     },
