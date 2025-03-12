f92933ae258c7836f18c7728b506eddb3de3c6e3
DalilaSam <dalila.sam.p.ts@gmail.com>
2025-03-12 14:46:55 +0000

    Darkmode home/favorites/cocktail

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div class="relative h-screen">
     <header>
       <div class="wrapper">
-        <nav class="p-4 bg-bg300 text-white">
+        <nav class="p-4 bg-bg300 text-white dark:bg-dbg300">
           <div class="flex justify-between items-center">
             <h1 class="md:hidden">Restaurante</h1>
             <button @click="() => showNav()" class="md:hidden p-2 bg-text100 rounded">
@@ -19,18 +19,22 @@
             </RouterLink>
             <RouterLink class="w-[30%] p-2 border-r-1 hover:bg-text200 rounded duration-300" to="/Favorites">
               {{ $t(`message.nav.fav`) }}</RouterLink>
-            <LanguageSwitch class="w-[30%] p-2 hover:bg-text200 rounded duration-300" />
+            <div class="w-[30%] p-2 hover:bg-text200 rounded duration-300">
+              <LanguageSwitch />
+              <button @click="changeTheme()">ddd</button>
+            </div>
           </div>
         </nav>
       </div>
     </header>
 
     <main class="z-30">
-    <RouterView/>
+      <RouterView />
 
     </main>
 
-    <footer class="collapse p-5 z-10 text-center flex justify-evenly bg-bg300 w-full fixed bottom-0 md:visible">
+    <footer
+      class="collapse p-5 z-10 text-center flex justify-evenly bg-bg300 w-full fixed bottom-0 md:visible dark:bg-dbg300 dark:text-dtext100">
       <div class="w-[30%] border-r-1">
         <p>Telf:34522345</p>
       </div>
@@ -53,6 +57,17 @@ const showMore = ref(false);
 
 const showNav = () => {
   showMore.value = !showMore.value;
+};
+
+const changeTheme = () => {
+  const theme = document.documentElement.classList;
+  if (theme.contains("dark")) {
+    theme.remove("dark");  // Cambia a tema claro
+    localStorage.setItem("theme", "light");
+  } else {
+    theme.add("dark");     // Cambia a tema oscuro
+    localStorage.setItem("theme", "dark");
+  }
 };
 </script>
 
